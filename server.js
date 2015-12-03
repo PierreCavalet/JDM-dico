68885fd92dca5f8391263e0d7e6072b21464cab0
cavalet.pierre@gmail.com <pierre.cavalet@etu.umontpellier.fr>
2015-12-03 16:51:34 +0100

    envoie de l'xml au client

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
-var fs = require("fs");
+var fs = require('fs');
+var parser = require('xml2json');
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/test', function(req, res) {
-	res.json({"foo": "bar"});
+	var readable = fs.createReadStream('chat.xml');
+	readable.on('open', function() { readable.pipe(res); });
+	readable.on('error', function() { res.end("[]"); });
 });
 
 app.listen(8888);
