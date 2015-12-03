73f44e78872a0200f91fab815fba4e7726143dce
cavalet.pierre@gmail.com <pierre.cavalet@etu.umontpellier.fr>
2015-12-03 15:55:12 +0100

    Distribution des fichiers du repertoire public par le serveur Node.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var fs = require("fs");
 var app = express();
 
-app.get('/test', function(req, res) {
+app.use(express.static(__dirname + "/public"));
 
+app.get('/test', function(req, res) {
+	
 });
 
 app.listen(8888);
