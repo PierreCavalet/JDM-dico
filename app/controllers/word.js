992fd8712abf45175ca21e1432ddd36c6982a7ba
Pierre Cavalet <pierre.cavalet@gmail.com>
2016-01-14 17:31:20 +0100

    clean up

--- app/controllers/word.js
+++ app/controllers/word.js
@@ -14,24 +14,34 @@ exports.getWord = function(req, res) {
         if (err)
             console.log(err);
 
-        // if a word was found in the db
         if (word) {
+            // word from database
             console.log("word load from cache");
             res.json(word);
         } else {
+            // word from jeuxdemots
             console.log("word load from jeuxdemots");
+
+            // url to request a word
             var url = 'http://www.jeuxdemots.org/rezo-xml.php?gotermsubmit=Chercher&output=onlyxml&gotermrel=' + word_mot;
-            request({uri:url, encoding: null}, function (error, response, body) {
+
+            // request with no encoding so the result is a buffer
+            request({uri : url, encoding :  null}, function (error, response, body) {
+                // convert from iso-8859-1 to utf-8
                 var iconv = new Iconv('iso-8859-1', 'utf-8');
                 var buffDecode = iconv.convert(body);
                 var stringDecode = buffDecode.toString('utf-8');
-                var temp = stringDecode.split("<CODE>");
 
+                // retrieve the content between the code markup
+                var temp = stringDecode.split("<CODE>");
+                // if there is no code markup
                 if(temp.length < 2) {
                     res.end();
                 } else {
                     var temp2 = temp[1].split("</CODE>");
                     var xmlToParse =  temp2[0];
+
+                    // parse xml to json
                     parser.parseString(xmlToParse, function (err, result) {
                         if (err) {
                             console.log(err);
@@ -60,6 +70,7 @@ exports.getWord = function(req, res) {
     });
 };
 
+// clean the json created with the xml
 function JSONcleanUp(JSONdata) {
     JSONdata.entrant = JSONdata.entrant[0].rel;
     JSONdata.sortant = JSONdata.sortant[0].rel;
