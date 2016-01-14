c7e23df882b94f09eec584fc36ffb1384a6711f2
Pierre Cavalet <pierre.cavalet@gmail.com>
2016-01-14 10:20:00 +0100

    gestion des cartes

--- public/js/controllers/WordCtrl.js
+++ public/js/controllers/WordCtrl.js
@@ -3,10 +3,27 @@ angular.module('WordCtrl', []).controller('WordController', function($scope, $ht
 	$http.get('/relations.json').then(function(response) {
 		jsonRelations = response.data;
 	});
-	$http.get('/words').then(function(response) {
+	$http.get('/api/words/chat').then(function(response) {
 		jsonWord = response.data;
 		console.log(jsonWord);
-		$scope.name = jsonWord["mot-formate"][0];
+
+        var association = [];
+        angular.forEach(jsonWord.entrant, function(value, key) {
+            if(typeof association[value.association.type] === 'undefined'){
+                association[value.association.type] = [];
+            };
+          association[value.association.type].push(value.mot);
+        });
+        console.log(association);
+
+        $scope.cards = ['test'];
+        console.log(association['r_action_lieu']);
+        for (var key in association) {
+            $scope.cards.push({
+                "title" : key,
+                "data" : association[key]
+            });
+        }
 	});
 
     $scope.tagline = 'Nothing beats a pocket protector!';
